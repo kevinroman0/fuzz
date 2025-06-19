d6287144e9861080aebb1255e3479cff55a4ceed
yasoofios <yasminemunozzz@gmail.com>
2025-06-19 12:57:41 -0500

    diff api loaded to db

--- exported_data/BOOF.py
+++ exported_data/BOOF.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import requests
 import json
@@ -14,23 +16,66 @@ uri = "mongodb+srv://romankevin176:d2ew7cN9U3VMV0Yj@fuzz.vkfskgo.mongodb.net/?re
 client = MongoClient(uri)  # default local MongoDB
 db = client['weather_db']  # Database name
 collection = db['history_weather']  # Collection name
+collection.delete_many({})  # Clear the collection before inserting new data
 
-csv = 'converted_timestamps.csv'
+csv = 'sorted_by_year_ca.csv'
 output_file = 'out.csv'
 
 df_input = pd.read_csv(csv)
 
+
+
+# api limit has 
+
 for _, row in df_input.iterrows():
     lat = row['incident_latitude']
     lon = row['incident_longitude']
-    start_date = row['incident_date_created']
-    end_date = row['incident_date_last_update']
+    name = row['incident_name']
+    start_date = row['incident_date_created'].split(' ')[0]
+    end_date = row['incident_date_last_update'].split('T')[0]
+    listy = ",".join([
+    "temperature_2m",
+    "relative_humidity_2m",
+    "dew_point_2m",
+    "apparent_temperature",
+    "pressure_msl",
+    "surface_pressure",
+    "cloudcover",
+    "cloudcover_low",
+    "cloudcover_mid",
+    "cloudcover_high",
+    "windspeed_10m",
+    "windgusts_10m",
+    "winddirection_10m",
+    "shortwave_radiation",
+    "direct_radiation",
+    "diffuse_radiation",
+    "direct_normal_irradiance",
+    "terrestrial_radiation",
+    "precipitation",
+    "rain",
+    "snowfall",
+    "weathercode",
+    "et0_fao_evapotranspiration",
+    "vapour_pressure_deficit",
+    "soil_temperature_0cm",
+    "soil_temperature_6cm",
+    "soil_temperature_18cm",
+    "soil_temperature_54cm",
+    "soil_moisture_0_1cm",
+    "soil_moisture_1_3cm",
+    "soil_moisture_3_9cm",
+    "soil_moisture_9_27cm",
+    "soil_moisture_27_81cm"
+])  
     
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&start={start_date}&end={end_date}&type=hour&appid=3459df5824c519bea0f8fa026299b5ef' 
     
+    # url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&start={start_date}&end={end_date}&type=hour&appid=3459df5824c519bea0f8fa026299b5ef' 
+    url = f'https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly={listy}&timezone=America/Los_Angeles'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
+        data['incident_name'] = name    #adds the incident name
         # json_str = json.dumps(data)
         collection.insert_one(data)
         
@@ -40,3 +85,9 @@ for _, row in df_input.iterrows():
         # df.to_csv(output_file, index=False, mode='a', header=not file_exists)
     else:
         print(f"Failed to fetch data for lat={lat}, lon={lon}. Status code: {response.status_code}")
+        print(f'{response.text}')
+
+        # main goal is to add the firename and make the overall identifier
+
+
+
